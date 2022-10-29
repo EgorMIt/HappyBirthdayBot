c1c9edbd6a6795c5be5aed1e4d3d6113118a0c7d
EgorMIt <egor.mitrofanov@hotmail.com>
2022-10-28 23:19:14 -0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,22 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
 
+echo -e "--------------- STOPPING ALL DOCKER CONTAINERS ---------------\n\n"
 # shellcheck disable=SC2046
 sudo docker stop $(sudo docker ps -aq)
+
+echo -e "--------------- DELETING ALL DOCKER CONTAINERS ---------------\n\n"
 # shellcheck disable=SC2046
 sudo docker rm $(sudo docker ps -aq)
 
+echo -e "-------------------- MAVEN BUILD RUNNING ---------------------\n\n"
 mvn -f pom.xml clean install -U
 
+echo -e "----------------- BUILDING DOCKER CONTAINER ------------------\n\n"
 sudo docker build -t my_bot .
 
+echo -e "------------------ RUNNING DOCKER CONTAINER ------------------\n\n"
 sudo docker run -d --restart=always --name=my_bot --network=host my_bot
+
+echo -e "---------------- RUNNING DOCKER LOGS FOLLOWING ---------------\n\n"
+docker logs -f my_bot
